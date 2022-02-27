ecda642faa5c5fced0ad53fd7c66267f77127f96
Princess Mortix <arthursilvacatitu@gmail.com>
2022-02-27 18:56:43 -0300

    PGO: Atualização de urgência (v1.0.0)

--- positivo.go
+++ positivo.go
@@ -22,7 +22,8 @@ type Solutions []struct {
 	Ativo    bool       `json:"ativo"`
 	Solucoes []Solucoes `json:"solucoes"`
 }
-type Solucoes struct {
+
+type Solucoes []struct {
 	ID               string `json:"id"`
 	Nome             string `json:"nome"`
 	Descricao        string `json:"descricao"`
@@ -34,6 +35,39 @@ type Solucoes struct {
 	Ordem            int    `json:"ordem"`
 	DataCadastro     string `json:"dataCadastro"`
 	Novo             bool   `json:"novo"`
+}
+
+type Userinfo struct {
+	Sub           string `json:"sub"`
+	AuthTime      int    `json:"auth_time"`
+	Idp           string `json:"idp"`
+	Name          string `json:"name"`
+	Username      string `json:"username"`
+	Email         string `json:"email"`
+	IntegrationID string `json:"integration_id"`
+	Amr           string `json:"amr"`
+	Schools       string `json:"schools"`
+}
+
+type School struct {
+	ID            string   `json:"id"`
+	IntegrationID string   `json:"integration_id"`
+	UserID        string   `json:"user_id"`
+	Name          string   `json:"name"`
+	Roles         []string `json:"roles"`
+	TimeZone      string   `json:"time_zone"`
+	URL           string   `json:"url"`
+}
+
+type UserClass []struct {
+	Value  string   `json:"value"`
+	Label  string   `json:"label"`
+	Turmas []Turmas `json:"turmas"`
+}
+type Turmas struct {
+	NomeTurma   string `json:"nomeTurma"`
+	TurmaValida bool   `json:"turmaValida"`
+	NomeSerie   string `json:"nomeSerie"`
 }
 
 type Error struct {
@@ -80,6 +114,42 @@ func Login(username string, password string) (string, error) {
 	return token.AccessToken, nil
 }
 
+func GetUserInfo(token string) (string, string, string, error) {
+	userinforesponse, err := tokenRequest("https://sso.specomunica.com.br/connect/userinfo", "GET", token)
+	if err != nil {
+		return "Um erro aconteceu:", "", "", err
+	}
+	var userinfo Userinfo
+	json.Unmarshal([]byte(userinforesponse), &userinfo)
+	//fmt.Println(userinfo)
+	//fmt.Println(userinfo.Schools)
+	var school School
+	json.Unmarshal([]byte(userinfo.Schools), &school)
+	//fmt.Println(school)
+	return school.ID, school.UserID, school.Roles[0], nil
+
+} //Retorna o id da escola, o id do usuário e o papel do usuário
+
+func GetClass(token string, userId string) (string, error) {
+	getclassresponse, err := tokenRequest("https://apihub.positivoon.com.br/api/NivelEnsino?usuarioId="+userId, "GET", token)
+	if err != nil {
+		return "", err
+	}
+	//unmarshal response
+	var userclass UserClass
+	json.Unmarshal([]byte(getclassresponse), &userclass)
+	return userclass[0].Value, nil
+
+}
+
+func GetRawSolutions(token string, role string, class string, schoolID string) (string, error) {
+	getRawSolutions, err := tokenRequest("https://apihub.positivoon.com.br/api/Categoria/Solucoes/Perfil/"+role+"?NivelEnsino="+class+"&IdEscola="+schoolID, "GET", token)
+	if err != nil {
+		return "", err
+	}
+	return getRawSolutions, nil
+}
+
 func GetHomework(token string) string {
 	return "https://plus-app.studos.com.br/auth/psd?jwt=" + token + "&redirect=/student/central"
 } //Retorna a url de acesso ao portal do aluno
@@ -98,16 +168,7 @@ func GetAgenda(token string) string {
 
 func GetBooks(token string) (string, error) {
 	return tokenRequest("https://livro-digital-estante.prd.positivoon.com.br/v3/livros?busca=&componenteCurricular=&nivelEnsino=&serie=&volume=", "GET", token)
-} //Retorna um json com os livros do aluno
-
-func GetUserinfo(token string) (string, error) {
-	userinforesponse, err := tokenRequest("http://sso.specomunica.com.br/connect/userinfo", "GET", token)
-	if err != nil {
-		return "Um erro aconteceu:", err
-	}
-	return userinforesponse, nil
-
-} //Retorna um json com os dados do usuário
+}
 
 func tokenRequest(url string, method string, token string) (string, error) {
 	client := &http.Client{}
